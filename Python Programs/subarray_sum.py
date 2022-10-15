cd8b38e53fcc441d4990c8cd55f800ef31ec6bdd
Dhanush Krishna R <dhanushkrishnaofficial@gmail.com>
2022-10-16 01:53:28 +0530

    Added py program for Largest Sum Contiguous Subarray

---
+++ Python Programs/subarray_sum.py
@@ -0,0 +1,22 @@
+n=int(input())
+def s(arr,start,end):
+    res=0
+    for j in arr:
+        res+=ar[j]
+    print(res)
+    return res
+for _ in range(n):
+    k=int(input())
+    ar=list(map(int,input().split()))
+    m=0
+    max_so_far = -1000
+    max_ending_here = 0
+    for i in range(0, k):
+        max_ending_here = max_ending_here + ar[i]
+        if (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
+ 
+        if max_ending_here < 0:
+            max_ending_here = 0
+    print(max_so_far)
+        
