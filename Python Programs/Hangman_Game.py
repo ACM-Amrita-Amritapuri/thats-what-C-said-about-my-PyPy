c857dab38ac3c78d119c7369458aaee608673cf9
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-28 23:00:11 +0530

    A hangman game program to guess the word.

---
+++ Python Programs/Hangman_Game.py
@@ -0,0 +1,126 @@
+import random
+
+word_list = [
+'abruptly', 
+'absurd', 
+'abyss', 
+'affix', 
+'askew', 
+'avenue', 
+'awkward', 
+'axiom', 
+'azure', 
+'bagpipes']
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+print(logo)
+print("You have 6 lives.")
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        
+        print(f"You guessed {guess}, that's not in the word. You lost a life.")
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lost! Better Luck Next Time!")
+            print(f'Pssst, the solution is {chosen_word}.')
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win! Well Done!")
+
+    stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+    print(stages[lives])
