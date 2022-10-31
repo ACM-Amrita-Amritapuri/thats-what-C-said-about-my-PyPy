9be3fab308f02f86304617fb333e1fa67f881da9
Aishwarya <86533885+Aicodes224@users.noreply.github.com>
2022-10-31 19:11:20 +0530

    Add files via upload

---
+++ C Programs/lab6q4s.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{   int n,i,j,s;
+    printf("enter number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+          for(j=1;j<=i;j++)
+{
+
+           for(j=1;j<=i;j++)
+           {printf("%d",\t");
+            printf("*");
+            }
+
+}
+
+          printf("\n");
+    }
+
+
+}
