cdffba7f4fdd07c5a63319e04431644460ca6584
Aishwarya <86533885+Aicodes224@users.noreply.github.com>
2022-10-31 19:25:15 +0530

    Create cnew3.c

---
+++ C Programs/cnew3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{   int n,i,j;
+    printf("enter number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+          for(j=1;j<=i;j++)
+{
+
+
+            printf("%d",j);
+
+}
+
+          printf("\n");
+    }
+
+
+}
